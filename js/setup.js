dc4a12169306cce61e3c809c1cfc2eff0930d543
kenziem67 <kenziem@sas.upenn.edu>
2025-04-03 15:31:10 -0400

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+  center: [39.951278, -75.19375],
+  zoom: 16,
+});
+
+var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
